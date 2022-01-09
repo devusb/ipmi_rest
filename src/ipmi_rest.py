0dd968bfc4a28d78f62df4c369520ce483686795
Morgan Helton <mhelton@superintendent.local>
2022-01-09 12:11:54 -0600

    move parameters to url and add Dockerfile

--- src/ipmi_rest.py
+++ src/ipmi_rest.py
@@ -1,9 +1,16 @@
 import pyipmi
 import pyipmi.interfaces
-import json
 from flask import Flask,jsonify,request
 
 
+def create_ipmi(hostname,port,username,password,target):
+    interface = pyipmi.interfaces.create_interface('rmcp')
+    connection = pyipmi.create_connection(interface)
+    connection.target = pyipmi.Target(int(target,16))
+    connection.session.set_session_type_rmcp(hostname, port=port)
+    connection.session.set_auth_type_user(username, password)
+    connection.session.establish()
+    return connection
 
 app = Flask(__name__)
 @app.route('/status')
@@ -14,15 +21,13 @@ def status():
     password = request.args.get("password")
     target = request.args.get("target")
 
-    interface = pyipmi.interfaces.create_interface('rmcp')
-    connection = pyipmi.create_connection(interface)
-    connection.target = pyipmi.Target(int(target,16))
-    connection.session.set_session_type_rmcp(hostname, port=port)
-    connection.session.set_auth_type_user(username, password)
-    connection.session.establish()
+    connection = create_ipmi(hostname,port,username,password,target)
 
-    return jsonify({'power_state': connection.get_chassis_status().power_on})
+    power_state = connection.get_chassis_status().power_on
+    connection.session.close()
 
+    return jsonify({'power_state': power_state})
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port="5001",debug=True)
 
