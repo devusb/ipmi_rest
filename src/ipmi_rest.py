f9ed5cc52a7f529e97f8ad5b5f11cb2cf43bfcf2
Morgan Helton <mhelton@superintendent.local>
2022-01-09 13:13:05 -0600

    move parameter extraction to function

--- src/ipmi_rest.py
+++ src/ipmi_rest.py
@@ -3,25 +3,26 @@ import pyipmi.interfaces
 from flask import Flask,jsonify,request
 
 
-def create_ipmi(hostname,port,username,password,target):
+def create_ipmi(request):
+    hostname = request.args.get("hostname")
+    port = request.args.get("port",type=int)
+    username = request.args.get("username")
+    password = request.args.get("password")
+    target = request.args.get("target")
+    
     interface = pyipmi.interfaces.create_interface('rmcp')
     connection = pyipmi.create_connection(interface)
     connection.target = pyipmi.Target(int(target,16))
     connection.session.set_session_type_rmcp(hostname, port=port)
     connection.session.set_auth_type_user(username, password)
     connection.session.establish()
+
     return connection
 
 app = Flask(__name__)
 @app.route('/status')
 def status():
-    hostname = request.args.get("hostname")
-    port = request.args.get("port",type=int)
-    username = request.args.get("username")
-    password = request.args.get("password")
-    target = request.args.get("target")
-
-    connection = create_ipmi(hostname,port,username,password,target)
+    connection = create_ipmi(request)
 
     power_state = connection.get_chassis_status().power_on
     connection.session.close()
@@ -30,13 +31,7 @@ def status():
 
 @app.route('/power_on')
 def power_on():
-    hostname = request.args.get("hostname")
-    port = request.args.get("port",type=int)
-    username = request.args.get("username")
-    password = request.args.get("password")
-    target = request.args.get("target")
-
-    connection = create_ipmi(hostname,port,username,password,target)
+    connection = create_ipmi(request)
 
     connection.chassis_control_power_up()
     connection.session.close()
@@ -45,13 +40,7 @@ def power_on():
 
 @app.route('/power_off')
 def power_off():
-    hostname = request.args.get("hostname")
-    port = request.args.get("port",type=int)
-    username = request.args.get("username")
-    password = request.args.get("password")
-    target = request.args.get("target")
-
-    connection = create_ipmi(hostname,port,username,password,target)
+    connection = create_ipmi(request)
 
     connection.chassis_control_soft_shutdown()
     connection.session.close()
